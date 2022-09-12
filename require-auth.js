4fb665f7fa7ed4867e8d59db03f7f980577e04f0
Bryan Pease <bcp015000@gmail.com>
2022-09-12 08:47:28 -0500

    Fixed syntax error

--- require-auth.js
+++ require-auth.js
@@ -1,7 +1,7 @@
 const { identifyUser } = require("./identify-user")
 const { forbidden } = require("./response")
 
-export const requireAuth = (fn) => {
+const requireAuth = (fn) => {
   return (event, context) => {
     const user = identifyUser(event, context);
     const userId = user?.data?.sub;
@@ -13,3 +13,5 @@ export const requireAuth = (fn) => {
     })
   }
 }
+
+module.exports = requireAuth
