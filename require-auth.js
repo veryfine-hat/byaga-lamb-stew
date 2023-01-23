d746f7c75c86a77cb5ca7c335152c40da6d58e7d
Bryan Pease <bcp015000@gmail.com>
2023-01-23 08:15:13 -0600

    Fixed incorrect applyTrace reading data from the wrong location in context

--- require-auth.js
+++ require-auth.js
@@ -2,7 +2,7 @@ const { identifyUser } = require("./identify-user")
 const { forbidden } = require("./response")
 
 const requireAuth = (fn) =>
-  (...argd) => {
+  (...args) => {
     const user = identifyUser();
     const userId = user?.data?.sub;
     if (!userId) return forbidden()
